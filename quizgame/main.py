1060284b8e728a6d38f94a15dfa9f78721a9338f
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2024-12-21 23:59:48 +0900

    show window

--- quizgame/main.py
+++ quizgame/main.py
@@ -1,2 +1,30 @@
 import pygame
 import numpy
+import sys
+from pygame.locals import *
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1280,960))
+    #フルスクリーンの場合
+    #screen = pygame.display.set_mode(SCREEN_SIZE,FULLSCREEN) 
+    pygame.display.set_caption('Hello,World')
+
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+if __name__=="__main__":
+    run()
